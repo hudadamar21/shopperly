e70cacf9e552fe54c80a736ac7a6c44a15582973
Levianth <74690464+LevianthProject@users.noreply.github.com>
2021-06-09 21:43:51 +0700

    Update index.vue

--- src/pages/Authentication/ForgetPassword/index.vue
+++ src/pages/Authentication/ForgetPassword/index.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="h-screen w-full grid place-items-center">
     <Back to="/"/>
-    <h1 class="brand text-8xl text-shopperly-green-400">Forget Password</h1>
+    <h1 class="brand text-8xl text-shopperly-green-400">Forget Password - Coming Soon</h1>
   </div>
 </template>
 
